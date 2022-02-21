f14893d18fea148f1604410deae6ea5e85831550
Parth  Patel <parth.patel@drcsystems.com>
2022-02-21 17:22:51 +0530

    fifth change

--- project/js/main.js
+++ project/js/main.js
@@ -1,9 +1,19 @@
-function showPicture(){
-  // use jQuery ($ is shorthand) to find the div on the page and then change the html
-  // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
-  $("p").html("High five! You're building your first web app!");
+function getWeather(searchQuery) {
+    $(".city").text("");
+    $(".temp").text("");
+    $(".error").text("");
+    var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&units=imperial&appid="+apiKey;
+    $.ajax(url,
+        {success: function(data){
+            $(".city").text(data.name);
+            $(".temp").text(data.main.temp);
+        }, error: function(error){
+            $(".error").text("An error occured.");
+        }}
+    );
+}
 
-  // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
-  
+function searchWeather() {
+    var searchQuery = $(".search").val(); 
+    getWeather(searchQuery);
 }
